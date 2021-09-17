b6c53d47089bd5d04560941c2654f927afcee35d
Shirley Almeida <ssh.lopes@gmail.com>
2021-09-17 17:48:04 -0300

    Arquivos incluidos

---
+++ quinzena1/lista-logica/out/exercicios.js
@@ -0,0 +1,116 @@
+// Exemplos
+
+// Exercício 0A
+function soma() {
+  // escreva seu código aqui
+  const num1 = prompt('Digite o primeiro número')
+  const num2 = prompt('Digite o segundo número')
+
+  console.log(Number(num1) + Number(num2))
+}
+
+// Exercício 0B
+function imprimeMensagem() {
+  // escreva seu código aqui
+  const mensagem = prompt('Digite sua mensagem')
+
+  console.log(mensagem)
+}
+
+// ---------------------------------------------------
+// Exercícios
+
+// Exercício 1
+function calculaAreaRetangulo() {
+  // escreva seu código aqui
+const altura = prompt("Qual a sua altura")
+const largura = prompt("Digite uma largura")
+
+console.log (altura*largura)
+
+}
+
+// Exercício 2
+function imprimeIdade() {
+  // escreva seu código aqui
+
+  const anoAtual = Number(prompt("Digite o ano atual"))
+  const idade = Number(prompt("Digite sua idade"))
+
+  console.log(anoAtual-idade)
+}
+
+// Exercício 3
+function calculaIMC() {
+  // escreva seu código aqui
+  const peso = Number(prompt("Digite seu peso:"))
+  const altura = Number(prompt("Digite sua altura:"))
+
+  console.log(altura/peso)
+
+}
+
+// Exercício 4
+function imprimeInformacoesUsuario() {
+  // escreva seu código aqui
+
+  const nome = prompt("Digite seu nome")
+  const idade = Number(prompt("Digite sua idade"))
+  const email = prompt("Digite seu email")
+
+  console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}`)
+  
+}
+
+// Exercício 5
+function imprimeTresCoresFavoritas() {
+  // escreva seu código aqui
+
+let cor1 = prompt("Digite sua cor favorita")
+let cor2 = prompt("Digite sua segunda cor favorita")
+let cor3 = prompt("Digite sua terceira cor favorita")
+
+cor1[0] 
+cor2[1] 
+cor3[2]
+
+console.log(cor1 , cor2, cor3)
+
+}
+
+// Exercício 6
+function retornaStringEmMaiuscula() {
+  // escreva seu código aqui
+const 
+
+}
+
+// Exercício 7
+function calculaIngressosEspetaculo() {
+  // escreva seu código aqui
+}
+
+// Exercício 8
+function checaStringsMesmoTamanho() {
+  // escreva seu código aqui
+}
+
+// Exercício 9
+function checaIgualdadeDesconsiderandoCase() {
+  // escreva seu código aqui
+}
+
+// Exercício 10
+function checaRenovacaoRG() {
+  // escreva seu código aqui
+}
+
+// Exercício 11
+function checaAnoBissexto() {
+  // escreva seu código aqui
+}
+
+// Exercício 12
+function checaValidadeInscricaoLabenu() {
+  // escreva seu código aqui
+}
