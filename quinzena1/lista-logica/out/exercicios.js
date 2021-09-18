8dd4968cadf5f48870a5317ecc3a2deb63ced64e
Shirley Almeida <ssh.lopes@gmail.com>
2021-09-17 23:48:28 -0300

    Exercícios Projeto

--- quinzena1/lista-logica/out/exercicios.js
+++ quinzena1/lista-logica/out/exercicios.js
@@ -45,8 +45,10 @@ function calculaIMC() {
   // escreva seu código aqui
   const peso = Number(prompt("Digite seu peso:"))
   const altura = Number(prompt("Digite sua altura:"))
+  const calculo1 = (altura * altura)
+  const imc = (peso / calculo1)
 
-  console.log(altura/peso)
+  console.log(imc)
 
 }
 
@@ -58,7 +60,7 @@ function imprimeInformacoesUsuario() {
   const idade = Number(prompt("Digite sua idade"))
   const email = prompt("Digite seu email")
 
-  console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}`)
+  console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
   
 }
 
@@ -70,47 +72,98 @@ let cor1 = prompt("Digite sua cor favorita")
 let cor2 = prompt("Digite sua segunda cor favorita")
 let cor3 = prompt("Digite sua terceira cor favorita")
 
-cor1[0] 
-cor2[1] 
-cor3[2]
+let cores = [cor1 , cor2, cor3]
 
-console.log(cor1 , cor2, cor3)
+console.log(cores)
 
 }
+
 
 // Exercício 6
 function retornaStringEmMaiuscula() {
   // escreva seu código aqui
-const 
+
+const palavra = prompt("Digite uma palavra")
+
+console.log(palavra.toUpperCase())
 
 }
 
 // Exercício 7
 function calculaIngressosEspetaculo() {
   // escreva seu código aqui
+let custo = Number(prompt("Qual o valor do ingresso?"))
+let valor = Number(prompt("Qual a quantidade de lugares?"))
+
+console.log(custo / valor)
+
 }
 
 // Exercício 8
 function checaStringsMesmoTamanho() {
   // escreva seu código aqui
+let string1 = prompt("Digite uma palavra:")
+let string2 = prompt("Digite outra palavra:")
+
+console.log(string1.length===string2.length)
+
 }
 
 // Exercício 9
 function checaIgualdadeDesconsiderandoCase() {
   // escreva seu código aqui
+  let palavra1 = prompt("Digite uma palavra:")
+  let palavra2 = prompt("Digite outra palavra:")
+
+  let palavras = palavra1.toLocaleUpperCase() === palavra2.toLocaleUpperCase()
+
+
+  console.log(palavras)
 }
 
 // Exercício 10
 function checaRenovacaoRG() {
   // escreva seu código aqui
+
+  let anoAtual = Number(prompt('Digite o ano atual: '))
+  let anoNasc = Number(prompt('Digite seu ano de nascimento: '))
+  let anoIdentidade = Number(prompt("Digite o ano de emissão da sua carteira de identidade: "))
+
+  let idade = anoAtual - anoNasc
+  let validadeIdentidade = anoAtual - anoIdentidade
+
+  let faixaEtaria1 = idade <= 20 && validadeIdentidade >= 5
+  let faixaEtaria2 = idade >= 20 && idade <= 50 &&  validadeIdentidade >= 10
+  let faixaEtaria3 = idade > 50 && validadeIdentidade >= 15
+
+  console.log(faixaEtaria1 || faixaEtaria2 || faixaEtaria3)
+  
 }
 
 // Exercício 11
 function checaAnoBissexto() {
   // escreva seu código aqui
+
+  const checagemAno = prompt("Digite um ano")
+
+  bissexto1 = (checagemAno % 400) == 0
+  bissexto2 = ((checagemAno % 4 == 0) && (checagemAno % 100 != 0))
+  bissextoNao = checagemAno && bissexto1 && bissexto2
+
+console.log (bissextoNao || bissexto1 || bissexto2)
+
+
 }
 
 // Exercício 12
 function checaValidadeInscricaoLabenu() {
   // escreva seu código aqui
+
+let idade = (prompt("Você tem mais de 18 anos? (sim / não"))
+let escolaridade = prompt("Você possui ensino médio completo?(sim /nao)")
+let horarios = prompt("Você possui disponibilidade exclusiva durante os horários do curso?")
+
+let respostas = idade === "sim" && escolaridade === "sim" && horarios === "sim"
+
+console.log(respostas)
 }
