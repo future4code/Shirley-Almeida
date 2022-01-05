d759d7e0cfb36dab3a54588dfc24a1ca6a8ef1d8
Shirley Almeida <ssh.lopes@gmail.com>
2022-01-04 22:48:11 -0300

    exercicios

--- modulo4/use-state/insta-lab-hooks/src/components/Post/Post.js
+++ modulo4/use-state/insta-lab-hooks/src/components/Post/Post.js
@@ -11,41 +11,74 @@ import iconeComentario from '../../img/comment_icon.svg'
 
 const Post = (props) => {
 
+  const [curtido, setCurtido] = useState(false)
+  const [numeroCurtidas, setNumeroCurtidas] = useState(0)
+  const [comentado, setComentado] = useState(false)
+  const [numeroComentarios, setNumeroComentarios] = useState(0)
+  const [comentarios, setComentarios] = useState([])
+
+
 
   const onClickCurtida = () => {
-  };
+    if (curtido) {
 
-  const onClickComentario = () => {
-  };
 
-  const enviarComentario = (comentario) => {
-  }
+      setCurtido(!curtido)
+      setNumeroCurtidas(numeroCurtidas - 1)
+    } else {
+      setCurtido(!curtido)
+      setNumeroCurtidas(numeroCurtidas + 1)
+    }
+  
+};
 
-  return (
-    <PostContainer>
-      <PostHeader>
-        <UserPhoto src={props.fotoUsuario} alt={'Imagem do usuario'}/>
-        <p>{props.nomeUsuario}</p>
-      </PostHeader>
+const onClickComentario = () => {
+  setComentado(!comentado)
+  
+};
 
-      <PostPhoto src={props.fotoPost} alt={'Imagem do post'}/>
-
-      <PostFooter>
-        <IconeComContador
-          // icone={iconeCurtida}
-          onClickIcone={onClickCurtida}
-          // valorContador={numeroCurtidas}
-        />
-
-        <IconeComContador
-          icone={iconeComentario}
-          onClickIcone={onClickComentario}
-          // valorContador={numeroComentarios}
-        />
-      </PostFooter>
-      {/* {caixaDeComentario} */}
-    </PostContainer>
-  )
+const enviarComentario = (comentario) => {
+  
 }
+const caixaDeComentario = comentado ? (
+<SecaoComentario enviarComentario={enviarComentario}/>
+    ) : (
+     
+      comentarios.map(comentario => {
+        return (
+          <CommentContainer>
+            <p>{comentario}</p>
+          </CommentContainer>
+        )
+      })
+    )
+const iconeCurtida = curtido ? (iconeCoracaoPreto) : (iconeCoracaoBranco)
 
+return (
+  <PostContainer>
+    <PostHeader>
+      <UserPhoto src={props.fotoUsuario} alt={'Imagem do usuario'} />
+      <p>{props.nomeUsuario}</p>
+    </PostHeader>
+
+    <PostPhoto src={props.fotoPost} alt={'Imagem do post'} />
+
+    <PostFooter>
+      <IconeComContador
+        icone={iconeCurtida}
+        onClickIcone={onClickCurtida}
+      // valorContador={numeroCurtidas}
+      />
+
+      <IconeComContador
+        icone={iconeComentario}
+        onClickIcone={onClickComentario}
+      // valorContador={numeroComentarios}
+      />
+    </PostFooter>
+    {/* {caixaDeComentario} */}
+
+  </PostContainer>
+) 
+  }
 export default Post
