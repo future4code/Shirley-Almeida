7c0e7ee952a636065f47dc99f64f6a435d747281
Shirley Almeida <ssh.lopes@gmail.com>
2022-02-20 19:24:13 -0300

    alterações

--- StarWars/projeto-starwars/src/App.js
+++ StarWars/projeto-starwars/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          STAR WARS
+          SHIRLEY 
         </p>
         <a
           className="App-link"
