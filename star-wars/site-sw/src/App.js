b59de90eb18ae4a96c0c38ee7688d3e7eeaaa5b8
Shirley Almeida <ssh.lopes@gmail.com>
2022-02-21 22:21:46 -0300

    inicio do projeto

---
+++ star-wars/site-sw/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CharacterDetailPage from "./pages/CharacterDetailPage/CharacterDetailPage";
+import CharacterListPage from "./pages/CharacterListPage/CharacterListPage";
+
+function App() {
+  return (
+    <div >
+      <CharacterDetailPage/>
+      <CharacterListPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ star-wars/site-sw/src/pages/CharacterListPage/CharacterListPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CharacterListPage = () => {
+    return [
+        <div>
+            <h1>CharacterListPage</h1>
+        </div>
+    ]
+}
+
+export default CharacterListPage
